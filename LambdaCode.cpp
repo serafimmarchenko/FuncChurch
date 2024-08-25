35fdd5efaa4fb9009248dfc0daf530ca4af3fbfe
serafimmarchenko <49483162+serafimmarchenko@users.noreply.github.com>
2024-08-25 18:37:39 +0300

    Update LambdaCode.cpp
    void run(...) complete

--- LambdaCode.cpp
+++ LambdaCode.cpp
@@ -6,11 +6,13 @@
 using namespace std;
 
 enum class TT {
-	Var = 0,
-	Const = 1,
-	Func = 2,
-	newFunc = 3,
-	Func = 4
+	Undef = 0,
+	Var = 1,
+	Const = 2,
+	Func = 3,
+	newFunc = 4,
+	nameFunc = 5,
+	setFunc = 6
 };
 
 class mTerm{
@@ -21,28 +23,30 @@ class mTerm{
 };
 map<string, mTerm*> terms;
 
-class mToken: public mTerm{
+class mToken : public mTerm{
 	public:
 		TT type;
 		vector<mTerm*> args;
 		mToken(string name, TT type) : mTerm::mTerm(name), type(type) {};
 };
 
-class Func: public mTerm{
+class Func : public mTerm{
 	public:
+		static string setName = "";
 		vector<mTerm*> args;
 		mToken body;
 		Func(string name, vector<mTerm*> args, mToken body) : mTerm(name), args(args), body(body) {};
+		string call(vector<mTerm*> args);
 };
 
 vector<mToken> lexer(string code) {
 	vector<mToken> res;
 	string word = "";
-	int num = 0;
-	TT type = TT::Var;
+	TT type = TT::Undef;
 	for (char c : code) {
 		if (isdigit(c)) {
 			num = num*10+(c-'0');
+			type
 		}
 		else if (c == 'L' && word == "") {
 			type = TT::newFunc;
@@ -59,9 +63,14 @@ mToken parse(vector<mToken> code) {
 	return res;
 }
 
+mToken* put_args(Func* f, vector<mTerm*> args) {
+	
+}
+
 void run(mToken* token, vector<Func>* funcs) {
 	switch(token->type){
 		case TT::Func:
+			token = static_cast<Func*>(token);
 			for(mTerm* a : token->args) {
 				if (token->type = TT::Var)
 					continue;
@@ -69,12 +78,29 @@ void run(mToken* token, vector<Func>* funcs) {
 					continue;
 				else if (token->type = TT::Func)
 					continue;
+				else if (token->type = TT::nameFunc)
+					continue;
 				else
 					*a = run(a, funcs);
 			}
-			return token->call();
+			new_token = put_args(token, token->args); // It's token, in which args was put on their places.
+			run(put_args(new_token->body, token->args), funcs);
+			delete new_token;
 		case TT::newFunc:
-			return mToken("bla", TT::Func);
+			if (token->args[0] == "set") {
+				(*funcs).push_back(Func(Func::setName, token->args[1], token->args[2]));
+			}
+			else {
+				(*funcs).push_back(Func(token->args[0], token->args[1], token->args[2]));
+			}
+			cout "#New func was made";
+		case TT::setFunc:
+			Func::setName = token->args[0];
+			run(token->args[1], funcs);
+			
+			cout << "#New func was set";
+		default:
+			cout << token->name;
 	}
 }
 
